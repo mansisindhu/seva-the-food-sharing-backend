e7dafa39f63057c83ac650ff9b1399b3a8d1a95c
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-01 19:04:34 +0530

    Dotenv added

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 module.exports = () => {
     return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ez6pn.mongodb.net/Seva?retryWrites=true&w=majority`);
 }
