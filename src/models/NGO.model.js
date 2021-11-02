06aba87e2353e1dd388c8a2cf1bc5ebe36541547
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-02 22:08:06 +0530

    NGO model and schema created

--- src/models/NGO.model.js
+++ src/models/NGO.model.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-const productsSchema = new mongoose.Schema({
-    mobile: { type: Number, required: false },
-    password: { type: String, required: false },
-}, {
-    timestamps: true
-});
+const NGOSchema = new mongoose.Schema({
+    NGOName: {type: String, required: true},
+    reviews: {type: Number, required: true},
+    totalCampaigns: {type: Number, required: true},
+    totalFeeds: {type: Number, required: true},
+    about: {type: String, required: true},
+    image: {type: String, required: true},
+    needFood: {type: Boolean, required: true},
+    totalVolunteers: {type: Number, required: true}
+})
 
-
-module.exports = mongoose.model("NGO", productsSchema);
+module.exports = mongoose.model("ngos", NGOSchema);
