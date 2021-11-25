921c45fd552a8e3e26fb01c7fca3184d6b5d1224
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-25 23:17:23 +0530

    --amend

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-module.exports = () => {
-    return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ez6pn.mongodb.net/Seva?retryWrites=true&w=majority`);
-}
+module.exports = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ez6pn.mongodb.net/Seva?retryWrites=true&w=majority`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const connect = require("./configs/db");
 const PORT = process.env.PORT || 9900;
 
 app.listen(PORT, async () => {
+    console.log("starting");
     await connect();
     console.log("listening to port", PORT);
 })
