4f97fc6898df4fc3d1387e90a5006d5ef20a7e77
Mansi Sindhu <mansisindhu2909@gmail.com>
2021-11-25 23:03:18 +0530

    Changes for deployment

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -1,41 +1,44 @@
 require("dotenv").config();
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const passport = require("passport");
 
 const User = require("../models/user.model");
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:9900/auth/google/callback"
-},
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+    },
     async function (accessToken, refreshToken, profile, cb) {
-        const email = profile?._json?.email;
-        const name = profile?._json?.name;
-        const picture = profile?._json?.picture;
-        let user;
-        try {
-            user = await User.findOne({ email }).lean().exec();
+      const email = profile?._json?.email;
+      const name = profile?._json?.name;
+      const picture = profile?._json?.picture;
+      let user;
+      try {
+        user = await User.findOne({ email }).lean().exec();
 
-            if (!user) {
-                user = await User.create({
-                    email: email,
-                    password: uuidv4(),
-                    name: name,
-                    profilePic: picture,
-                    donations: []
-                })
-            }
-
-            return cb(null, user)
-        } catch (err) {
-            console.log({ err });
+        if (!user) {
+          user = await User.create({
+            email: email,
+            password: uuidv4(),
+            name: name,
+            profilePic: picture,
+            donations: [],
+          });
         }
+
+        return cb(null, user);
+      } catch (err) {
+        console.log({ err });
+      }
     }
-));
+  )
+);
 
 module.exports = passport;
--- src/index.js
+++ src/index.js
@@ -6,76 +6,94 @@ const app = express();
 app.use(express.json());
 
 const session = require("express-session");
-const redis = require("redis");
 
-const RedisStore = require("connect-redis")(session);
-const redisClient = redis.createClient();
+if (process.env.IS_HEROKU) {
+  app.set("trust proxy", 1);
+  app.use(
+    session({
+      secret: process.env.SECRET_KEY,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.FRONTEND_URL,
+        path: "/",
+        httpOnly: true,
+      },
+    })
+  );
+} else {
+  const redis = require("redis");
+  const RedisStore = require("connect-redis")(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: process.env.SECRET_KEY,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+}
 
 const cors = require("cors");
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
 
 const User = require("./models/user.model");
-
 const userController = require("./controllers/user.controller");
 const ngoController = require("./controllers/ngo.controller");
 
 const passport = require("./configs/passport");
-
-app.use(session({
-    store: new RedisStore({ client: redisClient }),
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: true
-}));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-    done(null, user._id);
+  done(null, user._id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    const newUser = await User.findById(id).lean().exec();
-    done(null, newUser);
+  const newUser = await User.findById(id).lean().exec();
+  done(null, newUser);
 });
 
 app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"],
-    })
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
 );
 
 app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: "/auth/google/failure",
-    }),
-    function (req, res) {
-        res.redirect("http://localhost:3000")
-    }
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  function (req, res) {
+    res.redirect(process.env.ORIGIN);
+  }
 );
 
 const isAutheticated = (req, res, next) => {
-    if (req.session.passport) {
-        next();
-    } else {
-        res.status(403).send({});
-    }
-}
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(401).send({});
+  }
+};
 
 app.get("/test", isAutheticated, (req, res) => {
-    res.send({ user: req.user || null })
-})
+  res.send({ user: req.user || null });
+});
 
 app.use("/user", isAutheticated, userController);
 
 app.use("/ngos", ngoController);
 
 app.get("/logout", (req, res) => {
-    req.logout();
-    res.send("logout");
+  req.logout();
+  res.send("logout");
 });
 
 module.exports = app;
